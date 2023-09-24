81b1078c7e2886dda5cd9adc3f5a6e92bd0a3cf1
Esteban Sepulveda <tebansd@icloud.com>
2023-09-24 10:37:11 -0700

    refactor prop data to context

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import './App.css';
 import { useEffect, useState } from 'react'
 import Gallery from './Components/Gallery.jsx';
 import SearchBar from './Components/SearchBar.jsx';
+import { DataContext } from './Context/DataContext'
 
 function App() {
   let [search, setSearch] = useState(''); 
@@ -35,7 +36,9 @@ function App() {
     <div className="App">
       <SearchBar handleSearch={handleSearch}  /> 
       {message}
-      <Gallery data={data}/> 
+      <DataContext.Provider value={data} >
+      <Gallery/> 
+      </DataContext.Provider>
     </div>
   );
 }
--- src/Components/Gallery.jsx
+++ src/Components/Gallery.jsx
@@ -1,7 +1,11 @@
 import GalleryItem from './GalleryItem';
+import { DataContext } from '../Context/DataContext.js'
+import { useContext } from 'react'
 
-export default function Galley(props) {
-    const display = props.data.map((item, index) => <GalleryItem item={item} index={index} />)
+export default function Galley() {
+    const data = useContext(DataContext)
+
+    const display = data.map((item, index) => <GalleryItem item={item} index={index} />)
 
     return (
         <div>

1d78b3e5310f498e0c54482c825640813102668d
Esteban Sepulveda <tebansd@icloud.com>
2023-09-26 19:43:27 -0700

    added and rendered artist and album view using the id

--- src/App.jsx
+++ src/App.jsx
@@ -1,57 +1,60 @@
 import './App.css';
-import { Fragment, useEffect, useState } from 'react'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import React, { useEffect, useState } from 'react';
+import { BrowserRouter as Router, Routes, Route, } from 'react-router-dom';
 import Gallery from './Components/Gallery.jsx';
 import SearchBar from './Components/SearchBar.jsx';
-import { DataContext } from './Context/DataContext'
 import AlbumView from './Components/AlbumView';
 import ArtistView from './Components/ArtistView';
+import { DataContext } from './Context/DataContext';
 
 function App() {
-  let [search, setSearch] = useState(''); 
-  let [data, setData] = useState([]);
-  let [message, setMessage] = useState('Search for Music!');
+  const [search, setSearch] = useState('');
+  const [data, setData] = useState([]);
+  const [message, setMessage] = useState('Search for Music!');
 
-  const API_URL = 'https://itunes.apple.com/search?term='
+  const API_URL = 'https://itunes.apple.com/search?term=';
 
   useEffect(() => {
     if (search) {
-    const fetchData = async () => {
-      document.title = `${search} Music`;
-      const response = await fetch(API_URL + search)
-      const resData = await response.json()
-      if (resData.results.length) {
-        setData(resData.results)
-      }else {
-        setMessage('Not Found')
-      }
+      const fetchData = async () => {
+        document.title = `${search} Music`;
+        const response = await fetch(API_URL + search);
+        const resData = await response.json();
+        if (resData.results.length) {
+          setData(resData.results);
+        } else {
+          setMessage('Not Found');
+        }
+      };
+      fetchData();
     }
-    fetchData();
-  }
-}, [search]); 
+  }, [search]);
 
   const handleSearch = (e, term) => {
-    e.preventDefault()
-    setSearch(term)
-  }
+    e.preventDefault();
+    setSearch(term);
+  };
 
   return (
     <div className="App">
       {message}
       <Router>
         <Routes>
-          <Route path = "/" element = {
-            <DataContext.Provider value={data} >
-            <Fragment>
-              <SearchBar handleSearch={handleSearch}  /> 
-              <Gallery data={data} /> 
-            </Fragment>
-            </DataContext.Provider>
-          } />
-            <Route path="/album/:id" element={<AlbumView />} />
-            <Route path="/artist/:id" element={<ArtistView />} />  
-         </Routes>
-        </Router>
+          <Route
+            path="/"
+            element={
+              <DataContext.Provider value={data}>
+                <React.Fragment>
+                  <SearchBar handleSearch={handleSearch} />
+                  <Gallery data={data} />
+                </React.Fragment>
+              </DataContext.Provider>
+            }
+          />
+          <Route path="/album/:id" element={<AlbumView />} />
+          <Route path="/artist/:id" element={<ArtistView />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
--- src/Components/ArtistView.jsx
+++ src/Components/ArtistView.jsx
@@ -1,14 +1,44 @@
-import { useState, useEffect } from 'react';
-import { useParams } from 'react-router-dom';
+import { useState, useEffect } from 'react'
+import { useParams, Link, useNavigate } from 'react-router-dom'
 
-export default function ArtistView() {
-  const { id } = useParams()
-  const [artistData, setArtistData] = useState([]);
+function ArtistView() {
+    const navigate = useNavigate()
+    const { id } = useParams()
+    const [ artistData, setArtistData ] = useState([])
 
-  return (
-    <div>
-      <h2>The id passed was: {id}</h2>
-      <p>Artist Data Goes Here!</p>
-    </div>
-  );
+    useEffect(() => {
+        const API_URL = `http://localhost:4000/album/${id}`
+        const fetchData = async () => {
+            const response = await fetch(API_URL)
+            const resData = await response.json()
+            setArtistData(resData.results)
+        }
+        fetchData()
+    }, [id])
+
+    const justAlbums = artistData.filter(entry => entry.collectionType === 'Album')
+
+    const renderAlbums = justAlbums.map((album, index) => {
+        return (
+            <div key={index}>
+                <Link to={`/album/${album.collectionId}`}>
+                    <p>{album.collectionName}</p>
+                </Link>
+            </div>
+        )
+    })
+
+    return (
+        <div>
+            <h2>{artistData.length ? artistData[0].artistName : 'Loading...'}</h2>
+            {renderAlbums}
+            <div>
+                <button onClick={() => navigate(-1)}>Back</button>
+                &nbsp;&nbsp;|&nbsp;&nbsp;
+                <button onClick={() => navigate('/')}>Home</button>
+            </div>
+        </div>
+    )
 }
+
+export default ArtistView
